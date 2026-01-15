e8888c8f8866f707f4855983c4579cb1a647c31f
Tristian Paquette <tdpaquette22@gmail.com>
2026-01-15 16:30:14 -0500

    added to test.c

--- test.c
+++ test.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int double_value(int x) {
+    return x * 2;
+}
+
+int main() {
+    printf("Enter an integer: ");
+    int num;
+    scanf("%d", &num);
+    int result = double_value(num);
+    printf("Double of %d is %d\n", num, result);
+    return 0;
+}
